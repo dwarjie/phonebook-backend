47e8229cb6c11aefaad18faa3da4ced1c84c1c58
dwarjie <macoycoder@gmail.com>
2025-02-10 22:27:20 +0800

    add: part3 - 3.6 exercise

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  if (!persons.length) {
+    return res.json({ message: "Person data is empty" });
+  }
+
+  res.json(persons);
+});
+
+app.get("/info/", (req, res) => {
+  if (!persons.length) {
+    return res.send(`<p>Person data is empty</p>`);
+  }
+
+  const template = `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>
+  `;
+  res.send(template);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (!person)
+    return res.status(400).json({ message: "Person ID is does not exist." });
+
+  res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (!person)
+    return res.status(400).json({ message: "Person ID is does not exist." });
+
+  const updatedData = persons.filter((person) => person.id !== id);
+  persons = updatedData;
+  res.json({ message: "Person successfully deleted." });
+});
+
+app.post("/api/persons/", (req, res) => {
+  const data = req.body;
+
+  if (
+    persons.find(
+      (person) => person.name.toLowerCase() === data.name.toLowerCase(),
+    )
+  )
+    return res.status(400).json({ message: "Name already exist." });
+
+  if (data.name == "" || data.number == "")
+    return res.status(400).json({ message: "Missing required informations." });
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 100),
+    name: data.name,
+    number: data.number,
+  };
+
+  persons = persons.concat(newPerson);
+  res.json({ message: `Person ${data.name} successfully created.` });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Listening in PORT: ${PORT}`);
+});
